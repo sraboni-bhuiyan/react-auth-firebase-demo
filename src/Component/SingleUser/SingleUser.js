8580043d2625ad2ad1fbc67bf573444a521e9560
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-04-16 01:19:20 +0200

    changes

--- src/Component/SingleUser/SingleUser.js
+++ src/Component/SingleUser/SingleUser.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SingleUser = ({user}) => {
+const SingleUser = ({user, children}) => {
 
     const {name, id} = user || {};
-
+    const {email, phone} = children || {};
 
     return (
         <div className="col-4 gy-2">
@@ -12,6 +12,8 @@ const SingleUser = ({user}) => {
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{id}</h6>
+                <p className='card-text'>{email}</p>
+                <p className='card-text'>{phone}</p>
                 <Link to={`/user/${id}`}>User Info</Link>
                 </div>
             </div>
--- src/Component/UserInfo/UserInfo.js
+++ src/Component/UserInfo/UserInfo.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useUsers from '../Hook/useUsers';
+import SingleUser from '../SingleUser/SingleUser';
 
 const UserInfo = () => {
-    let params = useParams();
-    console.log(params);
+    let {userId} = useParams();
+    
+    const allUsers = useUsers(`https://jsonplaceholder.typicode.com/users?id=${userId}`)
+
+
     return (
         <div>
-            user
+            <SingleUser user={allUsers[0]}>
+                {
+                    {email: allUsers[0]?.email,
+                    phone: allUsers[0]?.phone}
+                }
+            </SingleUser>
         </div>
     );
 };
